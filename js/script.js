92da44819cfc9ee36fc488dc5f924472468eea7d
Bay Bui <baybui@ucla.edu>
2022-07-20 19:27:26 -0700

    added prev and next button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 //When website is finished loading execute the following code ** activate superslides
 $(document).ready(function () {
-  $("#slides").superslides();
+  $("#slides").superslides({
+    animation: "fade",
+    play: 5000,
+    pagination: false,
+  });
 });
