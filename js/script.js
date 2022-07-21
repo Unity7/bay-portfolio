301fb65c8ec695434b6bc4d1aaedca1a554d552c
Bay Bui <baybui@ucla.edu>
2022-07-20 20:13:29 -0700

    added typed feature to headings

--- js/script.js
+++ js/script.js
@@ -5,4 +5,11 @@ $(document).ready(function () {
     play: 5000,
     pagination: false,
   });
+  var typed = new Typed(".typed", {
+    strings: ["Full Stack Web Developer", "Web3 Developer", "Data Analyst"],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false,
+  });
 });
